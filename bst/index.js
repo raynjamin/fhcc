8afe2c6d8e00a4994246878a53608a44b335adf5
Hanna <jhannablackwell22@gmail.com>
2021-01-28 21:25:50 -0500

    add insert, find, and depth functions

--- bst/index.js
+++ bst/index.js
@@ -1,6 +1,16 @@
 const { parse } = require('./parser');
-const entries = parse('./data.csv');
+const entries = parse('./data.csv'); // alt entries to test depth -> 10,5,8,3,26,17,38,42,7,25,33
 const TreeNode = require('./treenode');
 const { print } = require('./printer');
 
-print(TreeNode.createFromEntries(entries));
+var tree = TreeNode.createFromEntries(entries);
+print(tree);
+
+tree.find(3);
+tree.find(44);
+tree.find(37);
+tree.find(36);
+tree.find(2);
+tree.find(11); // NOPE
+tree.find(12); // NOPE. THE FURY IS REAL.
+console.log(tree.depth());
--- bst/treenode.js
+++ bst/treenode.js
@@ -3,17 +3,65 @@ function TreeNode(value, left, right) {
     this.left = left || null;
     this.right = right || null;
     this._pipes = [];
-    
+
+    /**
+     * Inserts a node into the tree.
+     */
     this.insert = function (node) {
-        // insert node into the tree (which is represented by just one root node)
+        if (node.value < this.value) {
+            if (this.left) {
+                this.left.insert(node);
+            } else {
+                this.left = node;
+            }
+        }
+
+        if (node.value > this.value) {
+            if (this.right) {
+                this.right.insert(node);
+            } else {
+                this.right = node;
+            }
+        }
     };
 
+    /**
+     * Returns the node with the matching value.
+     */
     this.find = function (value) {
-        // return the node with the matching value
+        if (value === this.value) {
+            console.log(this);
+        }
+
+        if (this.left && value < this.value) {
+            this.left.find(value);
+        }
+
+        if (this.right && value > this.value) {
+            this.right.find(value);
+        }
+
+        if (value !== this.value && !this.left && !this.right) {
+            console.log(`If you were hoping to find a ${value} I'm afraid you're barking up the wrong tree, friend.`)
+        }
     };
 
+    /**
+     * Gets the depth of this tree.
+     */
     this.depth = function () {
-        // get the depth of this tree (how far down does it go?)
+        let leftDepth = 0;
+        let rightDepth = 0;
+
+        if (this.left) {
+            leftDepth = this.left.depth() + 1;
+        }
+
+        if (this.right) {
+            rightDepth = this.right.depth() + 1;
+        }
+
+        return leftDepth > rightDepth ? leftDepth : rightDepth ;
     };
 
     // EXTRA CREDIT (hardmode)
